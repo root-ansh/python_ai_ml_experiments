3eb8ea4f13c37c598c28091dcab7dd963fd21dd5
= <=>
2018-10-08 00:58:58 +0530

    added scikit image processing, pandas, more numpy modules

---
+++ 04-10-18 [1] NUMPY/temp.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+n = np.arange(0, 50, 5)  # list of all numbers <=49 divisible by 5
+print(n)
+
+#
+# array = np.array([1,2,3,4,5,6])
+# print(array)
+#
+# a =np.ones((2,3),dtype=np.int16)
+# print(a)
+#
+# b= np.empty((2,3))
+# print(b)
+# k = range(20)
+# print(k)
+# for i in k:
+#     i[...] *= 2
+# print(k)
