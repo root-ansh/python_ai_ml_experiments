0ba9e684e817461e70af0912040c25acd9a6d9b6
= <=>
2018-10-12 04:53:07 +0530

    added histogram

--- 04-10-18 [1] NUMPY/temp.py
+++ 04-10-18 [1] NUMPY/temp.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-n = np.arange(0, 50, 5)  # list of all numbers <=49 divisible by 5
-print(n)
+# n = np.arange(0, 50, 5)  # list of all numbers <=49 divisible by 5
+# print(n)
 
 #
 # array = np.array([1,2,3,4,5,6])
@@ -17,3 +17,25 @@ print(n)
 # for i in k:
 #     i[...] *= 2
 # print(k)
+
+"""
+n= np.random.choice([0,2,4],16).reshape(4,4)
+print(n)
+
+print(">>",type(np.mean(n,axis=1)),np.mean(n,axis=1))
+a=np.array([1,1,1,1])
+a=a.reshape([-1,1])
+print(a)
+print(n-a)
+"""
+
+my_arr= np.random.choice([0, 2], 16).reshape(4, 4)
+print(my_arr)
+mean_arr=np.mean(my_arr, axis=1) #  nX1 array with mean of each row
+mean_arr_T=mean_arr.reshape([-1,1]) #mean_arr converted to 1Xn dimension (vertical) array
+my_arr= my_arr - mean_arr_T # subtracting mean of each row from elements of each row
+
+
+print(my_arr)
+
+
